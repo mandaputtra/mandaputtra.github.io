3160a95a3cb3d7a3431cc1c471ece9860daae34a
Manda Putra <mandaputra8@gmail.com>
2020-02-27 19:32:28 +0700

    the right plugins options is this

--- gridsome.config.js
+++ gridsome.config.js
@@ -1,16 +1,29 @@
-const tailwindcss = require('tailwindcss')
-const purgecss = require('@fullhuman/postcss-purgecss')
-
 module.exports = {
   siteName: 'mandaputtra',
-  plugins: [],
-  css: {
-    loaderOptions: {
-      postcss: {
-        plugins: [
-          tailwindcss
-        ]
-      }
-    }
+  chainWebpack: config => {
+    config.module
+      .rule('css')
+      .oneOf('normal')
+      .use('postcss-loader')
+      .tap(options => {
+        options.plugins.unshift(...[
+          require('tailwindcss')
+        ])
+
+        if (process.env.NODE_ENV === 'production') {
+          options.plugins.push(...[
+            require('@fullhuman/postcss-purgecss')({
+              content: [
+                'src/assets/**/*.css',
+                'src/**/*.vue',
+                'src/**/*.js'
+              ],
+              defaultExtractor: content => content.match(/[\w-/:%]+(?<!:)/g) || []
+            })
+          ])
+        }
+
+        return options
+      })
   }
 }
--- src/pages/rig.vue
+++
@@ -1,3 +0,0 @@
-<template>
-  <h1>RiiIGGG</h1>
-</template>
